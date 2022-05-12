b3eea2056ebab3ea6af8f3328bd0f86d45c8d0fe
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-05-12 18:18:25 -0500

    Agregando nuevas rutas (parametros y objetos)

--- app.js
+++ app.js
@@ -5,7 +5,28 @@ const port = 3000 //localhost 3000
 
 //app.get('/', (request (peticion), response (respuesta)) => {}) '/' : raiz = localhost 3000
 app.get('/', (req,res) => {
-    res.send("Hola a todos explorers")
+    res.send("Hola a todos explorers - Hello world with express")
+})
+
+app.get('/launchX', (req,res) => { //creando una url 
+    res.send("Hello world with LaunchX & express")
+})
+
+app.get('/node', (req,res) => { //creando una url 
+    const explorers0 = {name:"Brian Ulises", username:"BrianOrihuelaP", age:22}
+    const explorers1 = {name:"Felipe", username:"Felip-ta", age:21}
+    //creo objetos 
+    const lista = [explorers0,explorers1]
+    //creo una lista con los objetos
+    res.send(lista)
+    //envia una respuesta al cliente con la info de la lista    
+})
+
+app.get('/params:params', (req,res) => { //creando una url que pide un parametro ":"
+    console.log(req.params);
+    //req.params => parametro de la peticion
+    res.send(req.params) //{ params: ':brian' }
+    //envia el parametro de la peticion 
 })
 
 app.listen(port , () => { 
